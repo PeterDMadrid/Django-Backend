71ed67fd3658dcd98365bf9c1950a63f2344ae9f
Peter Madrid <petermadrid0421@gmail.com>
2024-11-17 08:03:44 +0800

    serializer

---
+++ accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import CustomUser, ProfilePicture
+
+class ProfilePictureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfilePicture
+        fields = ('id', 'username', 'image')
+
+class UserSerializer(serializers.ModelSerializer):
+    profile_picture = ProfilePictureSerializer(read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'profile_picture', 'level')
