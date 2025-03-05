7ed392578dafa7248c7f4950af19d57c0b5cdac2
Peter Madrid <petermadrid0421@gmail.com>
2025-03-06 03:01:06 +0800

    secret key

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ung#ze=!y5ejr&+0^br=7_bv4+i9my+n$7w%_3my3zbsbyv_c'
+import os
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
